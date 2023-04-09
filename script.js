3a46f4445611b16c55ad34663d79604e5c0685f5
Richy <richymbiamou52@gmail.com>
2023-04-09 12:52:54 +0100

    mise en place des fichiers

---
+++ script.js
@@ -0,0 +1,7 @@
+let labels = document.querySelectorAll('form label')
+labels.forEach(label =>{
+    label.innerHTML = label.innerText
+    .split('')
+    .map((letter, idx) => `<span style="transition-delay: ${idx * 50}ms">${letter}</span>`)
+    .join('')
+})
